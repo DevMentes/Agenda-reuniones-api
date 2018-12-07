de5906864fb741c9597e1a66fece940186b16f3f
Camilo Alaniz <kmilo93sd@gmail.com>
2018-12-07 15:22:32 -0300

    setup for client

--- src/Infrastructure/Slim/bootstrap/databaseConfig.php
+++ src/Infrastructure/Slim/bootstrap/databaseConfig.php
@@ -6,24 +6,10 @@ define('DB_CONFIG', array(
 	'host' => 'localhost',
 	'database' => 'agenda_reuniones_ciisa',
 	'username' => 'root',
-	'password' => '000000',
+	'password' => '',
 
 	'charset' => 'utf8',
 	'collation' => 'utf8_unicode_ci',
 	'prefix' => '',
 	)
 );
-
-/*define('DB_CONFIG', array(
-
-        'driver' => 'mysql',
-        'host' => 'us-cdbr-iron-east-01.cleardb.net',
-        'database' => 'heroku_15439a59e65e020',
-        'username' => 'b9f9a4726d1502',
-        'password' => '72ba9b70',
-
-        'charset' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix' => '',
-    )
-);*/
